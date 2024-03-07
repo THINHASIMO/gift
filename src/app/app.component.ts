2b0da94d53f2c834f00f874a5c1e16fb2c2d7e17
OWS-Thinhpv <thinhpv@ows.com.vn>
2024-03-07 22:06:45 +0700

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,27 @@ export class AppComponent {
   ngOnInit() {
     const textContainer: any = document.getElementById('textAnimation');
     const text = textContainer.innerText;
-
     textContainer.innerHTML = '';
 
     for (let i = 0; i < text.length; i++) {
       const span = document.createElement('span');
       span.textContent = text[i];
-      span.style.opacity = '0';
+      span.style.color = 'red';
+      span.style.opacity = '1'; // Set opacity to 1 to keep the text visible
       span.style.animation = `fadeInText 1s forwards cubic-bezier(0.25, 0.1, 0.25, 1) ${
         i * 0.1
       }s`;
       textContainer.appendChild(span);
 
-      // Kiểm tra nếu là ký tự cuối cùng của một từ
+      // Check if it's the last character of a word
       if (text[i] === ' ' || i === text.length - 1) {
         setTimeout(() => {
           const heart = document.createElement('span');
-          heart.style.color = 'red'; // Thiết lập màu chữ thành đỏ
+          heart.style.color = 'red';
           heart.textContent = '❤';
           heart.style.animation = 'flyHeart 1.5s linear forwards';
           textContainer.appendChild(heart);
-        }, i * 100); // Thêm trái tim sau mỗi từ, mỗi trái tim cách nhau 0.1 giây
+        }, i * 100);
       }
     }
   }
